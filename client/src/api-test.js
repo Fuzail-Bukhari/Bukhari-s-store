c4d2a2765843b78b2ff38140efa0ceac831aa26e
Fuzail Bukhari <bukharifuzail58@gmail.com>
2026-03-13 01:23:08 -0700

    Fix template literal syntax in api-test.js

--- client/src/api-test.js
+++ client/src/api-test.js
@@ -1,7 +1,7 @@
 ﻿console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
 
 // Test API connection
-fetch(${import.meta.env.VITE_API_URL}/products)
+fetch(`${import.meta.env.VITE_API_URL}/products`)
   .then(res => res.json())
   .then(data => console.log('API Test:', data))
   .catch(err => console.error('API Test Failed:', err));
