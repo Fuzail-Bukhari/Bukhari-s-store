ad5269bf2fb5c15d697457b36d1d36229f803a54
Fuzail Bukhari <bukharifuzail58@gmail.com>
2026-03-12 23:33:53 -0700

    Prepare frontend for Vercel deployment with API config and vercel.json

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,4 @@
-import axios from "axios";
+﻿// API configuration file for Vercel deployment
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-export default api;
+export default API_URL;
